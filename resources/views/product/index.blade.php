5a81376a64ab8e7bbc5046e20de727ae90181073
duyliem <duyliemapt@gmail.com>
2017-08-22 12:12:57 +0700

    Init project

---
+++ resources/views/product/index.blade.php
@@ -0,0 +1,14 @@
+<table border="1">
+    <tr>
+        <th>ID</th>
+        <th>Name</th>
+    </tr>
+    @if(isset($products))
+        @foreach($products as $item)
+            <tr>
+                <td>{{ $item->id }}</td>
+                <td>{{ $item->name }}</td>
+            </tr>
+        @endforeach
+    @endif
+</table>
